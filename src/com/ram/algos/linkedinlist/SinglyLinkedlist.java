183f450c0c66bf94ab1f30fc9588d7e97fbf462c
ramakanth <ramakanth6789@gmail.com>
2022-01-18 11:45:30 +0530

    Insert and traversal

--- src/com/ram/algos/linkedinlist/SinglyLinkedlist.java
+++ src/com/ram/algos/linkedinlist/SinglyLinkedlist.java
@@ -17,13 +17,62 @@ public class SinglyLinkedlist {
         return head;
     }
 
-    public void insertInLinkedinList(int nodeValue, int location){
+    public void insertInLinkedinList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createSinglyLinkedList(nodeValue);
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
 
     }
 
+    public void traversalLinkedinList() {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+        } else {
+            Node tempHead = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempHead.value);
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempHead = tempHead.next;
+            }
+            System.out.println();
+        }
+    }
 
-
-
-    
+    public boolean searchValue(int nodeValue) {
+        if (head != null) {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                if (tempNode.value == nodeValue) {
+                    System.out.println("Found at position: " + (i + 1));
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("Value not found.");
+        return false;
+    }
 
 }
