38177747601739189bd720f62a9ae828ff45f9dd
ramakanth <ramakanth6789@gmail.com>
2022-01-20 11:39:27 +0530

    Circular linked list

--- src/com/ram/algos/linkedlist/circular/CircularLinkedList.java
+++ src/com/ram/algos/linkedlist/circular/CircularLinkedList.java
@@ -14,4 +14,45 @@ public class CircularLinkedList {
         size = 1;
         return head;
     }
+
+    public void insertLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createLinkedlist(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+            tail.next = head;
+        } else if (location >= size) {
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location-1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+        }
+        size++;
+
+    }
+
+    public void traverseLinkedList() {
+        Node node = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(node.value);
+            if (i != size - 1) {
+                System.out.print(" -> ");
+            }
+            node = node.next;
+        }
+        System.out.println();
+    }
+
 }
--- src/com/ram/algos/linkedlist/circular/Main.java
+++ src/com/ram/algos/linkedlist/circular/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         CircularLinkedList circularLinkedList = new CircularLinkedList();
         Node head = circularLinkedList.createLinkedlist(5);
+        circularLinkedList.insertLinkedList(1,2);
+        circularLinkedList.insertLinkedList(3, 3);
+        circularLinkedList.insertLinkedList(6, 4);
         System.out.println(head.value);
-        System.out.println(head.next.value);
+        System.out.println(head.next.next.value);
+        System.out.println("------");
+
+        circularLinkedList.traverseLinkedList();
     }
 }
