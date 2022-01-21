14ebab841788a09290a36e1a8901ffc270661fa4
ramakanth <ramakanth6789@gmail.com>
2022-01-21 11:01:48 +0530

    Circular linked list

--- src/com/ram/algos/linkedlist/circular/Main.java
+++ src/com/ram/algos/linkedlist/circular/Main.java
@@ -12,5 +12,8 @@ public class Main {
         System.out.println("------");
 
         circularLinkedList.traverseLinkedList();
+        circularLinkedList.searchNode(6);
+        circularLinkedList.deleteNodeLinkedinList(3);
+        circularLinkedList.traverseLinkedList();
     }
 }
