53e8889f406ecee8f6085223bc7325af18feb368
ramakanth <ramakanth6789@gmail.com>
2022-01-26 12:55:00 +0530

    Stack

---
+++ src/com/ram/algos/stack/StackMain.java
@@ -0,0 +1,24 @@
+package com.ram.algos.stack;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.peek();
+        stack.isFull();
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+    }
+}
